cb5713611fecad1910e2abd044bcb242aab66bfd
Joemar Bagalanon <jbagalanon@gmail.com>
2021-06-02 13:58:10 +0800

    manual setup of an array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this picture while biking today.',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: "Here's the picture of the mountain.",
+    },
+
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'bikinglover',
+      content: 'I saw this picture while biking today.',
+    },
+
+    {
+      title: 'Hiking',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'hikinglover',
+      content: 'I saw this picture while biking today.',
+    },
+  ];
 }
